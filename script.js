4548564b4455907b152923b9e2074e139f85c61d
dharul619 <dharulyuswansyah619@gmail.com>
2023-09-12 13:54:06 +0700

    Commit Awal

---
+++ script.js
@@ -0,0 +1,15 @@
+// Togle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("aktif");
+};
+
+// klik diluar sidebar untuk menghilangkan bar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("aktif");
+  }
+});
